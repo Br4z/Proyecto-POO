35ef6d873c4111dc23be5711939a3e06d0f41f29
JuanGetial <getialps4@gmail.com>
2022-03-09 18:24:56 -0500

    Se agregaron comentarios

--- SOURCE FILES/Lugares/EXTENDS FROM LUGAR/Rio.cpp
+++ SOURCE FILES/Lugares/EXTENDS FROM LUGAR/Rio.cpp
@@ -28,12 +28,16 @@ Rio::~Rio()
 
 }
 
-//
+//Returna true si hay algun personaje en el rio, si no false
 bool Rio::alguienCayoAlRio()
 {
+    //Var auxiliar
     bool alguienCayoAlRio = false;
-    for(int i = 0; i<numPersonajes; i++)
+
+    //Se recorre cada puntero(personaje) del rio
+    for(int i = 0; i<numPersonajes; i++)//i representara el indice de cada personaje
     {
+        //Se verifica si el puntero(persoanje) es diferente de nullptr, es decir, si hay un persoanje
         if(personajes[i] != nullptr)
         {
             cout<<personajes[i]->getNombre()<<" cayo al rio"<<endl;

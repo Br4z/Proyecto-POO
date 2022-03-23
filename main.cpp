39cabdc54d6d375c3b724c91f4eb97962beb55ee
JuanGetial <getialps4@gmail.com>
2022-03-23 12:28:12 -0500

    Juego Final

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
+
+/*
+Archivo: main.cpp
+Autores: 
+Getial Juan Sebastian 
+<getial.juan@correounivalle.edu.co>
+Calderón Prieto Brandon
+<bcalderonprieto@gmail.com>
+Huertas Cadavid Nicolas Fernando
+<nicolas.huertas@correounivalle.edu.co>
+Fecha Creacion: 2022-03-19
+Fecha Ultima Modificacion: 2022-03-19
+Licencia: GNU-GPL
+*/
 #include <iostream>
 #include <string.h>
 #include "HEADER FILES/Jugador.h"
 using namespace std;
+
+/*
+Historia:Hay un robot que debe transportar un zorro, un conejo y una lechuga desde un lado del 
+río hasta la otra orilla, usando una barca. En la barca solo cabe uno de los tres individuos, 
+además del robot. El problema es que si el robot deja solos al zorro y al conejo, el zorro se 
+comerá el conejo. Y si deja solos al conejo y la lechuga, el conejo se comerá la lechuga. 
+El jugador debe controlar qué órdenes dar, para lograr que el robot transporte los tres 
+individuos a la otra orilla, sanos y salvos.
+*/
 
 int main()
 {
